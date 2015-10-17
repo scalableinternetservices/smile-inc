3e5e85ac4f9aae1a221e1f69a95c253bd96edaae
jasonwooCH <smwlks@hotmail.com>
2015-10-16 18:12:35 -0700

    Added Chroma controller to have access to both Moods and Mood Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'chroma/index'
+
   resources :mood_updates
   resources :moods
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  root 'chroma#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
