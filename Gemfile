3f14982af262915fd657a14e8a49169958c21dc3
jen mac <jnfmac@gmail.com>
2015-10-20 10:40:43 -0700

    added username and description to db, adjusted registration view

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ group :development do
 end
 
 gem 'devise'
+gem 'bootstrap-sass', '3.2.0.0'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.create!(username: "coolguy1234", email: "ex4@google.com", description: "pumpkin spice and everything nice", password: "pumpkin123", password_confirmation: "pumpkin123" )
+  end
+
+  test "should be a valid username" do
+    @user.username = "  "
+    assert_not @user.valid?
+  end
+
+  test "should have something under description" do
+    @user.description = " "
+    assert_not @user.valid?
+  end
+  
 end
