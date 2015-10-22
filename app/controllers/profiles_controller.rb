da1c160a42940e9fac5d5693ff85b0572da1f69d
jen mac <jnfmac@gmail.com>
2015-10-22 15:25:43 -0700

    fixed fixture errorand added integration test to check for page existence

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,9 @@ class ProfilesController < ApplicationController
 
   def edit
     @user=User.find(params[:id])
+    if @user.update_attributes(params.permit(:username, :email, :password, :password_confirmation))
+      #redirect somewhere
+    end
   end
-  
+    
 end
--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -6,4 +6,9 @@ class ProfileControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be able to access edit page from profile page" do
+    get :edit_profile_page
+    assert_response :success
+  end
+  
 end
