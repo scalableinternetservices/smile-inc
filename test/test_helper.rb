062a754ada924a4f974057b98752c09e501e5e87
jasonwooCH <smwlks@hotmail.com>
2015-10-22 18:37:08 -0700

    fixed link test cases

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+class ActionController::TestCase
+      include Devise::TestHelpers
+end
+
   # Add more helper methods to be used by all tests here...
 end
