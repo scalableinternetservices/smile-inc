abfae57be27bd91470e5000da3885d7bbc2c244e
jen mac <jnfmac@gmail.com>
2015-10-26 13:23:05 -0700

    Fixed pathway bug conflict with resource users

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   get 'chroma/index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
- #For profile to update user
+  #For profile to update user
   get '/profiles/:id', :to => 'profiles#show', :as => :user
 
   resources :profiles
   resources :mood_updates
   resources :moods
-  devise_for :users
 
   root 'chroma#index'
 
