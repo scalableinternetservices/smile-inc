b645a4cd6d075bc3abcff9f822ab5742ebc5c8e7
jen mac <jnfmac@gmail.com>
2015-10-28 19:49:16 -0700

    changed all views to accept username and overrid the email as the id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,33 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+              :case_sensitive => false
+            }
+  #secure validation of username
+  def validate_username
+    if User.where(email: username).exist?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  
+  #fix for mysql ignoring index in the email col
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["username = :value OR lower(email) = lower(:value)", { :value => downcase }]).first
+    else
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+    end
+  end
+  
 end
