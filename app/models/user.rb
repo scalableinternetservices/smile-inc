12800b8f346da1917dd7881628cb4e9bb2fae1f0
jasonwooCH <smwlks@hotmail.com>
2015-10-29 22:46:17 -0700

    Follower and following show up on profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,5 +42,30 @@ class User < ActiveRecord::Base
       where(conditions.to_hash).first
     end
   end
+
+  # FOLLOWER FEATURE RELATION
+  has_many :active_relationships, class_name:  "Relationship",
+  	   			  foreign_key: "follower_id",
+				  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name:  "Relationship",
+  	   			   foreign_key: "followed_id",
+				   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
+  # FOLLOWER FUNCTIONS
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
   
 end
