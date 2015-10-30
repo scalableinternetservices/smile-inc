64d1a107f01d86d2483fcd12262ed5382a75d50d
jasonwooCH <smwlks@hotmail.com>
2015-10-30 16:16:20 -0700

    test cases and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,12 @@ Rails.application.routes.draw do
              }
   resources :profiles
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030002439) do
+ActiveRecord::Schema.define(version: 20151030034844) do
 
   create_table "mood_updates", force: :cascade do |t|
     t.integer  "mood_id"
@@ -42,6 +42,17 @@ ActiveRecord::Schema.define(version: 20151030002439) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
