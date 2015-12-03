e4fb8dd42ae97f30bf03807d82e9c9333868e497
fnumono <wangpramono@gmail.com>
2015-12-02 20:34:54 -0800

    evertyhing ready for tsung test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,54 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Users
-# User.create!(username: "Example User",
-#              email:    "example@testing.org",
-# 	     password: "examplepin")
+#Users
+
 
 100.times do |n|
-  username = Faker::Name.name
+
+  colors = "red"
+  moods = "angry"
+
+  lol = Mood.create!(color: colors , mood: moods)
+  
+  lol.save!
+end
+
+100.times do |n|
+
+  id = rand(1..100)
+  intense = rand(1..10)
+  k = MoodUpdate.create(mood_id: id,
+                            desc: "blah",
+                            intensity: intense,
+                            created_at: "2015-11-#{n} 06:18:17")
+  k.save!
+
+end
+
+10000.times do |n|
+
+  bodies = "lol"
+  m = rand(1..100)
+  c = Comment.create(body: bodies, mood_update_id: m)
+
+  c.save!
+
+end
+
+100.times do |n|
+  username = "ryan#{n+1}"
   email    = "example-#{n+1}@testing.org"
   password = "examplepin"
   user = User.create(username: username,
                      email:    email,
                      password: password )
-  aboutme = Faker::Company.catch_phrase
-  fname = Faker::Name.first_name
-  lname = Faker::Name.last_name
-  user.profile.update(first_name: fname,
-                      last_name:  lname,
-                      about_me:   aboutme )
+  aboutme = "im good"
+  fname = "fnu"
+  lname = "lnu"
+  # user.profile.update(first_name: fname,
+  #                     last_name:  lname,
+  #                     about_me:   aboutme )
   image = rand(1..5)
   # path = "#{Rails.root}/public/images/#{image}.jpeg"
   # user.profile.avatar = File.open(path)
@@ -43,40 +73,29 @@
                             created_at: "2015-11-#{n} 06:18:17")
     user.save!
   end
-  
+ 
+
+
 end
 
-100.times do |n|
+a = User.create!(username: "Example User",
+             email:    "example@testing.org",
+       password: "examplepin")
 
-  colors = Faker::Commerce.color
-  moods = Faker::Name.name
+k = a.mood.create!(color: "red",
+                       mood: "moody")
 
-  lol = Mood.create!(color: colors , mood: moods)
-  
-  lol.save!
-end
-
-100.times do |n|
-
-  id = rand(1..100)
-  intense = rand(1..10)
-  k = MoodUpdate.create(mood_id: id,
+5.times do |n|
+    intense = rand(1..10)
+    a.mood_update.create(mood_id: k.id,
                             desc: "blah",
                             intensity: intense,
                             created_at: "2015-11-#{n} 06:18:17")
-  k.save!
+    a.save!
+  end
 
-end
 
-100.times do |n|
 
-  bodies = Faker::Hacker.adjective
-  m = rand(1..100)
-  c = Comment.create(body: bodies, mood_update_id: m)
-
-  c.save!
-
-end
 
 # Following relationships
 users = User.all
